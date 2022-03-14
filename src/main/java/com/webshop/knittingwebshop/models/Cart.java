4a1a70d23ec86e91dcab73b820893feb426fe774
AntraMi <antra.mikelsone@gmail.com>
2022-03-14 19:49:05 +0100

    cart changes

--- src/main/java/com/webshop/knittingwebshop/models/Cart.java
+++ src/main/java/com/webshop/knittingwebshop/models/Cart.java
@@ -1,10 +1,15 @@
 package com.webshop.knittingwebshop.models;
 
 import lombok.Data;
+import javax.persistence.*;
 
+@Entity
+@Table(name = "orders")
 @Data
 public class Cart {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String price;
@@ -18,4 +23,6 @@ public class Cart {
         this.quantity = quantity;
         this.image = image;
     }
+
+    public Cart() {}
 }
